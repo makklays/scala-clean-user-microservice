2ac63a4e970aa6a7e41918c1bbde1154e503beb3
Alexander <alexander@makklays.com.ua>
2026-02-15 01:45:16 +0100

    15-02-2026 first commit

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,6 +1,15 @@
 package com.techmatrix18
 
+/**
+ * The main entry point of the application.
+ *
+ * @author Alexander Kuziv <makklays@gmail.com>
+ * @company TechMatrix18
+ * @since 15.02.2026
+ * @version 0.0.1
+ */
 @main
 def main(): Unit = {
   println("Hello world!")
 }
+
